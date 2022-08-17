b0c5f86d16f42dcd06ed0b90c5541604c79cd544
Qiuhan Zhou <qiuhanzhou@gmail.com>
2022-08-17 15:37:32 -0400

    add category-item component

---
+++ src/Components/category-item/category-item.js
@@ -0,0 +1,21 @@
+export default function CategoryItem({ category }) {
+  //define category object which is ready to be received from outside
+
+  //destructure the variable inside category object to be received
+  const { title, imageUrl } = category
+
+  return (
+    <div className='category-container'>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='category-body-container'>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  )
+}
