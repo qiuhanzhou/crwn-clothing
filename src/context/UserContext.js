4173e11cc7b0009d74d6fb47e3d4fb5db5f5f9db
Qiuhan Zhou <qiuhanzhou@gmail.com>
2023-04-12 23:57:21 -0400

    update user context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -9,40 +9,40 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 })
 
-
 function currentUserReducer(state, action) {
-  switch (action.type) {
-    case 'set_current_user': {
-      return {
-        ...state,
-        currentUser:payload
-      }
-    }
-    default: {
-      throw Error('Unknown action: ' + action.type);
-    }
-  }
+  // switch (action.type) {
+  //   case 'set_current_user': {
+  //     return {
+  //       ...state,
+  //       currentUser: payload,
+  //     }
+  //   }
+  //   default: {
+  //     throw Error('Unknown action: ' + action.type)
+  //   }
+  // }
 }
 
-
 export const UserProvider = ({ children }) => {
-  // const [currentUser, setCurrentUser] = useState(null)
-  // const value = { currentUser, setCurrentUser }
+  const [currentUser, setCurrentUser] = useState(null)
+  const value = { currentUser, setCurrentUser }
 
   //converting useState to useReducer
-  const [currentUser, dispatch] = useReducer(currentUserReducer, initialCurrentUser);
+  // const [currentUser, dispatch] = useReducer(
+  //   currentUserReducer,
+  //   initialCurrentUser,
+  // )
 
-  function setCurrentUser(user) {
-    dispatch(
-      // "action" object:
-      {
-        type: 'set_current_user',
-        payload: user
-      }
-    );
-  }
+  // function setCurrentUser(user) {
+  //   dispatch(
+  //     // "action" object:
+  //     {
+  //       type: 'set_current_user',
+  //       payload: user,
+  //     },
+  //   )
+  // }
 
-function setCurrentUser()
   //centralizing current user setter
   useEffect(() => {
     const unsubsribe = onAuthStateChangedListener((user) => {
